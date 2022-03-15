1327749f77640f4184c77a62529018b1e51a39d6
mev <mev@PC-19-18>
2022-03-15 17:50:55 +0100

    Vježbe15.03

---
+++ Controllers/Student.cs
@@ -0,0 +1,38 @@
+﻿using Projektt.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Projektt.Controllers
+{
+    public class Student : Controller
+    {
+        // GET: Student
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Početna o studentima";
+            ViewBag.Fakultet = "Međimursko veleučilište";
+            return View();
+        }
+        public ActionResult Popis()
+        {
+            StudentiDB studentDB = new StudentiDB(); //alt + enter
+            return View(studentDB);
+        }
+        public ActionResult Detalji (int? id)
+        {
+            if (id.HasValue)
+                StudentiDB studentiDB = new StudentiDB();
+
+            Student student = studentDB.VratiListu()
+                .FirsDefault(x => x.ID == id);
+            if(student ==null)
+            {
+
+            }
+                    return View(Student)
+        }
+    }
+}
---
+++ Models/StudentiDB.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Projektt.Models
+{
+    
+    public class StudentiDB
+    {
+        private List<Student> lista = new List<Student>();
+
+        public StudentiDB()
+        {  
+            lista.Add(new Student()
+            {
+                ID = 1,
+                Prezime = "Ivić",
+                Spol = 'M',
+                DatumRodjenja = new DateTime(1991, 02, 01),
+                Oib = "12345678901",
+                Ime = "Josip",
+                RedovanStudent = true
+
+            });
+                        lista.Add(new Student()
+                        {
+                            ID = 2,
+                            Prezime = "Sivić",
+                            Spol = 'M',
+                            DatumRodjenja = new DateTime(1991, 03, 01),
+                            Oib = "12345678910",
+                            Ime = "Ivo",
+                            RedovanStudent = true
+
+                        });
+            lista.Add(new Student()
+            {
+                ID = 3,
+                Prezime = "Andrić",
+                Spol = 'M',
+                DatumRodjenja = new DateTime(1991, 02, 02),
+                Oib = "12345678911",
+                Ime = "Ivan",
+                RedovanStudent = true
+
+            });
+            lista.Add(new Student()
+            {
+                ID = 4,
+                Prezime = "Mrvić",
+                Spol = 'M',
+                DatumRodjenja = new DateTime(1992, 02, 01),
+                Oib = "12345678913",
+                Ime="Petar",
+                RedovanStudent = true
+
+            });
+        }
+        public List<Student>VratiListu()
+        {
+            return lista;
+        }
+    }
+}
